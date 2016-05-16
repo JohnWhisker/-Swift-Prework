01e6f36cc2a4c81a0491b91c39ea4829419fa16c
JohnWhisker <nguyen.dk0952@sinhvien.hoasen.edu.vn>
2016-05-16 15:16:30 +0700

    Update: Finised basic app

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 Submitted by: **Dang Khoi Nguyen**
 
-Time spent: **2** hours spent in total
+Time spent: **6** hours spent in total
 
 ## User Stories
 
@@ -17,9 +17,9 @@ The following **required** functionality is complete:
 
 The following **optional** features are implemented:
 * [ ] UI animations
-* [ ] Remembering the bill amount across app restarts (if <10mins)
-* [ ] Using locale-specific currency and currency thousands separators.
-* [ ] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
+* [X] Remembering the bill amount across app restarts (if <10mins)
+* [X] Using locale-specific currency and currency thousands separators.
+* [X] Making sure the keyboard is always visible and the bill amount is always the first responder. This way the user doesn't have to tap anywhere to use this app. Just launch the app and start typing.
 
 The following **additional** features are implemented:
 
--- TipsCal/ViewController.swift
+++ TipsCal/ViewController.swift
@@ -7,41 +7,74 @@
 //
 
 import UIKit
-
+struct Number {
+    static let formatterWithSepator: NSNumberFormatter = {
+        let formatter = NSNumberFormatter()
+        formatter.groupingSeparator = ","
+        formatter.numberStyle = .DecimalStyle
+        return formatter
+    }()
+}
+extension IntegerType {
+    var stringFormatedWithSepator: String {
+        return Number.formatterWithSepator.stringFromNumber(hashValue) ?? ""
+    }
+}
 class ViewController: UIViewController {
-
-    @IBOutlet weak var billField: UITextField!
+       @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     @IBOutlet weak var tipSegment: UISegmentedControl!
-    
+    var tipPercentages = [0.18,0.2,0.22]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+      
+        billField.becomeFirstResponder()    
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let bill = defaults.doubleForKey("bill")
+        let selected = defaults.integerForKey("selected")
+        billField.text = reFormat(bill)
+        calAndSet(bill, selected: selected)
         
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
     }
+
+   
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     @IBAction func onEdittingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18,0.2,0.22]
-        
-        var billAmount = NSString(string: billField.text!).doubleValue
-        var tip = billAmount*tipPercentages[ tipSegment.selectedSegmentIndex]
-        var total = billAmount+tip
-        
-              
-        tipLabel.text = String.init(format: "$%.2f", tip)
-        totalLabel.text = String.init(format: "$%.2f", total)
+        let billAmount = NSString(string: billField.text!).doubleValue
+        calAndSet(billAmount, selected: -1)
     }
-
+    
+    func calAndSet(billAmount:Double,selected:Int){
+        if(selected != -1 ){
+            tipSegment.selectedSegmentIndex = selected
+        }        
+        let tip = billAmount * tipPercentages[ tipSegment.selectedSegmentIndex]
+        let total = billAmount+tip
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setDouble(billAmount, forKey: "bill")
+        defaults.setInteger(tipSegment.selectedSegmentIndex, forKey: "selected")
+        defaults.synchronize()
+        
+        tipLabel.text = reFormat(tip)
+        totalLabel.text = reFormat(total)
+    }
+    func reFormat(input:Double)->String  {
+        let formatter = NSNumberFormatter()
+        formatter.locale = NSLocale.currentLocale()
+        formatter.numberStyle = NSNumberFormatterStyle.CurrencyStyle
+        print(formatter.stringFromNumber(input)!)
+        return formatter.stringFromNumber(input)!
+        
+    }
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
     }
-
 }
 
